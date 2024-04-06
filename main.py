fea4056d699f00f76a1e65110fad618e84141c09
nischalshakya787 <nischalshakya787@gmail.com>
2024-04-06 11:26:07 +0545

    yt-video-downloader

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from pytube import YouTube
 
 yt = YouTube('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley') #replace the link which you want to download
 
-# all = yt.streams.all()
-# video = yt.streams.filter(type="video") #Filters only video
-audio = yt.streams.filter(only_audio = True) #Filters only mp3
-audio_list = list(enumerate(audio))
+# If you want to dowload the video you can uncomment the command with filter type = video and filter only_audio for mp3
 
-for i in audio_list:
+# type = yt.streams.all()
+# type = yt.streams.filter(type="video") #Filters only video
+type = yt.streams.filter(only_audio = True) #Filters only mp3
+type_list = list(enumerate(type))
+
+for i in type_list:
     print (i)
 
 stream = int(input('Enter the number you want to download: '))
 
-audio[stream].download('downloads/')
+type[stream].download('downloads/')
 
